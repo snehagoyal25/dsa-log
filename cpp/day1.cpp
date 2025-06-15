7904fefeb652ee9bb6ed325de33149fd4d1c5906
snehagoyal25 <goyalsneha995@gmail.com>
2025-06-15 21:01:58 +0530

    cpp

---
+++ cpp/day1.cpp
@@ -0,0 +1,25 @@
+// Include basics of cpp
+
+// to execute in terminal write -> for compilation = g++ day1.cpp
+// for output -> ./day1
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello world "<<endl;
+//     return 0;
+// }
+
+// Taking input 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n ;
+//     cout<<"Enter the number : ";
+//     cin>>n;
+//     cout<<"number entered is : "<<n<<endl;
+
+//     return 0;
+// }
+
