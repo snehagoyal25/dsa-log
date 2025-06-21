ec90963773803acc91efa205b4f08b3e0e1d3c2a
snehagoyal25 <goyalsneha995@gmail.com>
2025-06-21 21:56:39 +0530

    pointers

---
+++ pointers.cpp
@@ -0,0 +1,25 @@
+// We have memory location as a hexadecimal numbers which are from 0 to 9 and a to f -> total 16 decimals are there .
+
+
+// evrytime you run this code , it will give different address as a is a local variable.
+
+// #include<iostream>
+// using namespace std ;
+// int main(){
+//     int a = 10;
+//     cout<< &a<<endl;
+//     return 0;
+// }
+
+// This will give same address when you run the code as a variable is defined globally.
+
+// #include<iostream>
+// using namespace std ;
+// int a = 10 ;
+// int main(){
+//     cout<< &a<<endl;
+//     return 0;
+// }
+
+
+// Pointers = 
