f829a64b8e85c38b31bbca17f79ccbcf96cac4d8
snehagoyal25 <goyalsneha995@gmail.com>
2025-06-23 12:44:15 +0530

    array

--- Arrays/array.cpp
+++ Arrays/array.cpp
@@ -6,40 +6,40 @@
 // int marks[50] = {1,2,3};
 // int marks[] = {1,2,3};
 
-// #include<iostream>
-// using namespace std;
-// int main (){
-    // int marks[50];
-    // cout<<marks[1]<<endl; // 0
-    // cout<<marks[2]<<endl; // 0 or garbage value 
-    // cout<<marks[50]<<endl; // garbage value as index does not exists.
+#include<iostream>
+using namespace std;
+int main (){
+    int marks[50];
+    cout<<marks[1]<<endl; // 0
+    cout<<marks[2]<<endl; // 0 or garbage value 
+    cout<<marks[50]<<endl; // garbage value as index does not exists.
 
-    // int mark[50] = {1,2,3};
-    // cout<<mark[0]<<endl;  // 1
-    // cout<<mark[1]<<endl;  // 2 
-    // cout<<mark[2]<<endl;  // 3
-    // cout<<mark[3]<<endl;  // 0
+    int mark[50] = {1,2,3};
+    cout<<mark[0]<<endl;  // 1
+    cout<<mark[1]<<endl;  // 2 
+    cout<<mark[2]<<endl;  // 3
+    cout<<mark[3]<<endl;  // 0
 
-//     int markss[] = {1,2,3};
-//     cout<<markss[0]<<endl;  //1 
-//     cout<<markss[1]<<endl;  //2
-//     cout<<markss[2]<<endl;  //3
-//     cout<<markss[3]<<endl;  // garbage value 
-//     return 0;
-// }
+    int markss[] = {1,2,3};
+    cout<<markss[0]<<endl;  //1 
+    cout<<markss[1]<<endl;  //2
+    cout<<markss[2]<<endl;  //3
+    cout<<markss[3]<<endl;  // garbage value 
+    return 0;
+}
 
 // Output in an array 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int array[5]={1,2,3,4,5};
-//     int n = sizeof(array)/sizeof(int);
+#include<iostream>
+using namespace std;
+int main(){
+    int array[5]={1,2,3,4,5};
+    int n = sizeof(array)/sizeof(int);
 
-// for(int i=0;i<=n-1 ;i++){
-//     cout<<array[i]<<endl;
-// }
-//     return 0;
-// }
+for(int i=0;i<=n-1 ;i++){
+    cout<<array[i]<<endl;
+}
+    return 0;
+}
 
 // Input in an array. 
 #include<iostream>
@@ -50,8 +50,8 @@ int main (){
     cin>>n;
     // int arr[n]; // giving error but running so we will do dynamic memory allocation .
      int *arr = new int[n];
-    // int n =sizeof(arr)/sizeof(int);
-    // cout<<"Enter values of array : ";
+    int n =sizeof(arr)/sizeof(int);
+    cout<<"Enter values of array : ";
 for(int i=0;i<n;i++){
     cin>>arr[i];
 }
