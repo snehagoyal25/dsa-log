de436aa58fd38cbdd1cb8f9a18675bcb2ab8f590
snehagoyal25 <goyalsneha995@gmail.com>
2025-09-03 16:28:59 +0530

    maxSubarraySum

---
+++ questionArray/maxSubarray.cpp
@@ -0,0 +1,33 @@
+// Maximum subarray sum = need to find the sum of the maximum array-> can be done in 3 ways
+// Tricky when there is a negative number in the array. 
+// Brute force =  the simple approach 
+// optimized 
+// kadane's algorithm
+
+#include<iostream>
+using namespace std;
+
+void maxSubarraySum(int *arr,int n){
+    int maxSum; // we should not initialize the value of maxSum as it can be less than 0.
+    for(int start=0;start<n;start++){
+        for(int end=start;end<n;end++){
+            int currentSum = 0 ;
+            for(int i=start;i<=end;i++){
+                currentSum += arr[i];
+            }
+            cout<<currentSum<<",";
+            maxSum = max(maxSum,currentSum); // In cpp their is function (max) which compares two values 
+            // In this case it will compare the currentSum of every subarray to maxSum and will give the maxSubarray.
+        }
+        cout<<endl;
+    }
+    cout<<"Maximum subarray sum : "<<maxSum<<endl;
+}
+
+int main(){
+    int arr[]={2,-3,6,-5,4,2};
+    int n=sizeof(arr)/sizeof(int);
+
+    maxSubarraySum(arr,n);
+    return 0;
+}
